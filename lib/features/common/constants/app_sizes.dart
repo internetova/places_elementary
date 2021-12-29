984128fbd8741d93ba23bce060669bda560d4878
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2021-12-29 11:55:21 +0300

    add константы приложения

---
+++ lib/features/common/constants/app_sizes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+//ignore_for_file: public_member_api_docs
+/// Часто используемые размеры, значения, маленькие виджеты
+class AppSizes {
+  /// размеры элементов
+ static const double radiusCard = 12;
+ static const double radiusButtonSmall = 10;
+ static const double radiusButton = 12;
+ static const double radiusInput = 8;
+ static const double radiusSearchInput = 12;
+ static const double heightBigButton = 48;
+ static const double heightInput = 40;
+ static const double heightInputTextArea = 80;
+ static const double toolbarHeightStandard = 80;
+ static const double widthButtonAddNewCard = 177;
+ static const double radiusButtonAddNewCard = 24;
+ static const double cardSizeSquareImgBig = 72;
+ static const double splashRadiusSmall = 18;
+ static const double splashRadiusMedium = 28;
+
+  /// частые отступы
+ static const sizedBoxW4 = SizedBox(width: 4);
+ static const sizedBoxW8 = SizedBox(width: 8);
+ static const sizedBoxW12 = SizedBox(width: 12);
+ static const sizedBoxW16 = SizedBox(width: 16);
+
+ static const sizedBoxH4 = SizedBox(height: 4);
+ static const sizedBoxH8 = SizedBox(height: 8);
+ static const sizedBoxH12 = SizedBox(height: 12);
+ static const sizedBoxH16 = SizedBox(height: 16);
+ static const sizedBoxH24 = SizedBox(height: 24);
+ static const sizedBoxH40 = SizedBox(height: 40);
+
+  /// SplashScreen
+ static const double splashLogo = 160;
+
+  /// задержка
+ static const milliseconds300 = Duration(milliseconds: 300);
+ static const milliseconds400 = Duration(milliseconds: 400);
+ static const milliseconds500 = Duration(milliseconds: 500);
+ static const milliseconds700 = Duration(milliseconds: 700);
+ static const milliseconds1500 = Duration(milliseconds: 1500);
+ static const seconds1 = Duration(seconds: 1);
+ static const seconds2 = Duration(seconds: 2);
+ static const seconds3 = Duration(seconds: 3);
+ static const seconds4 = Duration(seconds: 4);
+ static const seconds5 = Duration(seconds: 5);
+
+ AppSizes._();
+}

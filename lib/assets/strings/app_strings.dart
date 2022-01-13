5f16f568cdc2c69d2aa6da3a7382ec7e6468bf5a
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-01-13 15:47:57 +0300

    clear AppStrings

--- lib/assets/strings/app_strings.dart
+++ lib/assets/strings/app_strings.dart
@@ -1,18 +1,5 @@
 /// Текстовые константы
 class AppStrings {
-  static const appTitle = 'Places';
-
-  /// Нижняя навигация
-  static const navPlacesLabel = 'Места';
-  static const navMapLabel = 'Карта';
-  static const navFavoritesLabel = 'Избранное';
-  static const navSettingsLabel = 'Настройки';
-
-  /// Ошибки
-  static const error = 'Ошибка';
-  static const errorEmptyHeader = 'Пусто';
-  static const errorEmptyText = 'Нет данных для отображения.';
-  static const errorSomethingsWrong = 'Что-то пошло не так.\nПопробуйте позже.';
 
   AppStrings._();
 }

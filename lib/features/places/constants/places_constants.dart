a81587dfecbd86eeba45a8eb107c4bc0eade71fa
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-01-14 14:17:13 +0300

    add CachedNetworkImage

--- lib/features/places/constants/places_constants.dart
+++ lib/features/places/constants/places_constants.dart
@@ -3,5 +3,8 @@ class PlacesConstants {
   static const double appBarHeight = 136;
   static const double sliverAppBarHeight = 136;
 
+  /// Высота изображения на карточке места
+  static const double heightImagePreview = 96;
+
   PlacesConstants._();
 }

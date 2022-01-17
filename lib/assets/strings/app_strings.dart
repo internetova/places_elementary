0bf9f7e7cf2074bb9f94296e80dfdc24c847bb0e
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-01-17 14:09:23 +0300

    add SharedPrefsStorage

--- lib/assets/strings/app_strings.dart
+++ lib/assets/strings/app_strings.dart
@@ -2,5 +2,16 @@
 class AppStrings {
   static const emptyString = '';
 
+  /// Ключи для SharedPrefsStorage
+  /// ключи для значений фильтра
+  static const keyRadius = 'key_radius';
+  static const keyTypeFilter = 'key_type_filter';
+
+  /// ключи для значений темы
+  static const keyThemeIsDark = 'key_theme_is_dark';
+
+  /// первый запуск приложения
+  static const keyOnboardingIsComplete = 'key_onboarding_is_complete';
+
   AppStrings._();
 }

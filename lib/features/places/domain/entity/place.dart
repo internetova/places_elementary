4a014c7450468d278278ba7a85681837bbd5efd5
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-01-26 22:43:08 +0300

    рефакторинг сущности Place

--- lib/features/places/domain/entity/place.dart
+++ lib/features/places/domain/entity/place.dart
@@ -1,14 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:places_elementary/features/places/domain/entity/card_type.dart';
 
 part 'place.freezed.dart';
 
 /// Места, отображаемые в программе
-/// добавляем поля:
-/// [isFavorite] - флаг для избранного
-/// [cardType] - флаг для кнопок действий на карточке (на поиске, в избранном)
-/// [date] - дата планирования или когда посетил, может быть null
-/// сохраняем в локальной базе данных
 @freezed
 class Place with _$Place {
   const factory Place({
@@ -20,8 +14,5 @@ class Place with _$Place {
     required String placeType,
     required String description,
     double? distance,
-    @Default(false) bool isFavorite,
-    @Default(CardType.search) CardType cardType,
-    DateTime? date,
   }) = _Place;
 }

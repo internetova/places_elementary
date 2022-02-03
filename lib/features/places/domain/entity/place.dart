b62d2dd721fb750496fff3cf26e2531f5fdecc1e
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-04 01:25:20 +0300

    рефакторинг сущность Place

--- lib/features/places/domain/entity/place.dart
+++ lib/features/places/domain/entity/place.dart
@@ -1,8 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'place.freezed.dart';
+part 'place.g.dart';
 
 /// Места, отображаемые в программе
+/// Методы сериализации для сохранения места в локальную базу данных
 @freezed
 class Place with _$Place {
   const factory Place({
@@ -15,4 +17,7 @@ class Place with _$Place {
     required String description,
     double? distance,
   }) = _Place;
+
+  factory Place.fromJson(Map<String, dynamic> json) =>
+      _$PlaceFromJson(json);
 }

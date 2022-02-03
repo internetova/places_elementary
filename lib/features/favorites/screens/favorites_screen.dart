709fa597319347c75199247af380cdd57898ef7c
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-04 01:39:53 +0300

    верстка и логика FavoritesScreen

--- lib/features/favorites/screens/favorites_screen.dart
+++ lib/features/favorites/screens/favorites_screen.dart
@@ -1,6 +1,10 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:places_elementary/features/favorites/domain/entity/favorite.dart';
+import 'package:places_elementary/features/favorites/domain/entity/favorite_type.dart';
 import 'package:places_elementary/features/favorites/screens/favorites_screen_wm.dart';
+import 'package:places_elementary/features/favorites/widgets/favorites_appbar.dart';
+import 'package:places_elementary/features/favorites/widgets/favorites_builder.dart';
 
 /// Экран Избранные места FavoritesScreen
 class FavoritesScreen extends ElementaryWidget<IFavoritesScreenWidgetModel> {
@@ -11,8 +15,29 @@ class FavoritesScreen extends ElementaryWidget<IFavoritesScreenWidgetModel> {
 
   @override
   Widget build(IFavoritesScreenWidgetModel wm) {
-    return const Center(
-      child: Text('FavoritesScreen'),
+    return Scaffold(
+      appBar: FavoritesAppBar(tabController: wm.tabController),
+      body: TabBarView(
+        controller: wm.tabController,
+        children: [
+          EntityStateNotifierBuilder<List<Favorite>>(
+            listenableEntityState: wm.plannedPlacesState,
+            builder: (_, data) => FavoritesBuilder(
+              data: data!,
+              favoriteType: FavoriteType.planned,
+              removeFromFavorites: wm.removeFavorite,
+            ),
+          ),
+          EntityStateNotifierBuilder<List<Favorite>>(
+            listenableEntityState: wm.visitedPlacesState,
+            builder: (_, data) => FavoritesBuilder(
+              data: data!,
+              favoriteType: FavoriteType.visited,
+              removeFromFavorites: wm.removeFavorite,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

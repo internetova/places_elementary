ffc73c09969b3f5ed99d732307f9c46e3445767b
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-04 23:03:08 +0300

    update FavoritesManager

--- lib/features/common/service/favorites_manager.dart
+++ lib/features/common/service/favorites_manager.dart
@@ -4,8 +4,18 @@ import 'package:elementary/elementary.dart';
 /// навигации обновлений экранов не происходит. Поэтому, пока буду учитывать это таким образом.
 class FavoritesManager {
   /// Меняем список избранного на экране Избранное
-   final favoritesState = StateNotifier<int>(initValue: 0);
+  final favoritesState = StateNotifier<int>(initValue: 0);
 
   /// Только что удалено
+  /// [int] - id места
   final removedState = StateNotifier<int?>();
+
+  /// Только что изменена дата посещения в избранных
+  /// [int] - id места
+  final changedDateState = StateNotifier<int?>();
+
+  /// Надо обновить строку если была обновлена дата
+  /// Т.к. на одной и той же карточке мы можем несколько раз менять дату, то будем учитывать
+  /// дополнительно клики
+  final needUpdateDateState = StateNotifier<int>(initValue: 0);
 }

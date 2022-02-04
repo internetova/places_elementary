2d790cc02783e70413a1b5ce53343c7b14fd8908
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-04 23:06:38 +0300

    update список кнопок действий

--- lib/features/favorites/widgets/for_card/card_actions_favorite.dart
+++ lib/features/favorites/widgets/for_card/card_actions_favorite.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:places_elementary/features/common/constants/app_sizes.dart';
+import 'package:places_elementary/features/favorites/domain/entity/favorite.dart';
+import 'package:places_elementary/features/favorites/domain/entity/favorite_type.dart';
+import 'package:places_elementary/features/favorites/widgets/reminder_favorites_button/reminder_favorites_button.dart';
 import 'package:places_elementary/features/favorites/widgets/remove_favorites_button/remove_favorites_button.dart';
-import 'package:places_elementary/features/places/domain/entity/place.dart';
 
 /// Кнопки действий на карточке Избранное
 class CardActionsFavorite extends StatelessWidget {
-  final Place place;
+  final Favorite favorite;
 
-  const CardActionsFavorite({Key? key, required this.place}) : super(key: key);
+  const CardActionsFavorite({Key? key, required this.favorite}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        RemoveFavoritesButton(place: place),
+        if (favorite.favoriteType == FavoriteType.planned)
+          ReminderFavoritesButton(favorite: favorite),
+        AppSizes.sizedBoxW8,
+        RemoveFavoritesButton(place: favorite.place),
       ],
     );
   }

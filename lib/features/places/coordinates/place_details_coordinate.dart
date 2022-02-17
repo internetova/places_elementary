d141fb2c56b3b7d9ffd70e291d0ae623d3bf498d
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-17 09:57:39 +0300

    refactor: координаты для существующих экранов

--- lib/features/places/coordinates/place_details_coordinate.dart
+++ lib/features/places/coordinates/place_details_coordinate.dart
@@ -1,20 +1,24 @@
 import 'package:places_elementary/features/navigation/domain/entity/coordinate.dart';
 import 'package:places_elementary/features/places/domain/entity/place.dart';
 import 'package:places_elementary/features/places/screens/place_details_screen/place_details_screen.dart';
+import 'package:places_elementary/features/places/widgets/test_coordinator/unique_screen/unique_screen.dart';
 
-class PlaceDetailsCoordinate implements Coordinate {
-  static const placeDetailsScreen = PlaceDetailsCoordinate._('place_details_screen');
+class PlaceDetailsCoordinate extends Coordinate {
+  static const placeDetailsScreen = PlaceDetailsCoordinate._('place_details_screen', false);
+  static const uniqueScreen = PlaceDetailsCoordinate._('unique_screen', true);
 
-  final String _value;
-
-  const PlaceDetailsCoordinate._(this._value);
-
-  @override
-  String toString() => _value;
+  const PlaceDetailsCoordinate._(
+    String value,
+    bool isUniqueCoordinate,
+  ) : super(
+          value: value,
+          isUniqueCoordinate: isUniqueCoordinate,
+        );
 }
 
 final Map<PlaceDetailsCoordinate, CoordinateBuilder> placeDetailsCoordinates = {
   PlaceDetailsCoordinate.placeDetailsScreen: (_, data) => PlaceDetailsScreen(
         place: (data as Map)['place'] as Place,
       ),
+  PlaceDetailsCoordinate.uniqueScreen: (_, __) => const UniqueScreen(),
 };

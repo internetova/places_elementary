e04069c301fe014551d23dd7c44ccc8fc98cbdb7
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-02-18 21:21:39 +0300

    refactor: небольшие изменения

--- lib/features/navigation/domain/entity/coordinate.dart
+++ lib/features/navigation/domain/entity/coordinate.dart
@@ -5,11 +5,11 @@ typedef CoordinateBuilder = Widget Function(BuildContext context, Object? data);
 
 /// Basic coordinate representation.
 abstract class Coordinate {
-  final bool isUniqueCoordinate;
   final String value;
+  final bool isUniqueCoordinate;
 
   const Coordinate({required this.value, required this.isUniqueCoordinate});
 
   @override
-  String toString() => '$value isUnique: $isUniqueCoordinate';
+  String toString() => value;
 }

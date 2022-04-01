8ecbd7909b5cddad1d45693dd8fb44ebb82f4364
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-04-01 17:21:40 +0300

    refactor: PlaceDetailsCoordinate

--- lib/features/places/coordinates/place_details_coordinate.dart
+++ lib/features/places/coordinates/place_details_coordinate.dart
@@ -2,15 +2,17 @@ import 'package:places_elementary/features/navigation/domain/entity/coordinate.d
 import 'package:places_elementary/features/places/domain/entity/place.dart';
 import 'package:places_elementary/features/places/screens/place_details_screen/place_details_screen.dart';
 
-class PlaceDetailsCoordinate implements Coordinate {
-  static const placeDetailsScreen = PlaceDetailsCoordinate._('place_details_screen');
+class PlaceDetailsCoordinate extends Coordinate {
+  static const placeDetailsScreen =
+      PlaceDetailsCoordinate._('place_details_screen', false);
 
-  final String _value;
-
-  const PlaceDetailsCoordinate._(this._value);
-
-  @override
-  String toString() => _value;
+  const PlaceDetailsCoordinate._(
+    String value,
+    bool isUnique,
+  ) : super(
+          value: value,
+          isUnique: isUnique,
+        );
 }
 
 final Map<PlaceDetailsCoordinate, CoordinateBuilder> placeDetailsCoordinates = {

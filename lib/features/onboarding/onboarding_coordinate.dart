b63b6ab940891d84c72e1734080a4040a2adac07
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-04-01 17:21:30 +0300

    refactor: OnboardingCoordinate

--- lib/features/onboarding/onboarding_coordinate.dart
+++ lib/features/onboarding/onboarding_coordinate.dart
@@ -1,15 +1,17 @@
 import 'package:places_elementary/features/navigation/domain/entity/coordinate.dart';
 import 'package:places_elementary/features/onboarding/screens/onboarding_screen/onboarding_screen.dart';
 
-class OnboardingCoordinate implements Coordinate {
-  static const onboardingScreen = OnboardingCoordinate._('onboarding_screen');
+class OnboardingCoordinate extends Coordinate {
+  static const onboardingScreen =
+      OnboardingCoordinate._('onboarding_screen', true);
 
-  final String _value;
-
-  const OnboardingCoordinate._(this._value);
-
-  @override
-  String toString() => _value;
+  const OnboardingCoordinate._(
+    String value,
+    bool isUnique,
+  ) : super(
+          value: value,
+          isUnique: isUnique,
+        );
 }
 
 final Map<OnboardingCoordinate, CoordinateBuilder> onboardingCoordinates = {

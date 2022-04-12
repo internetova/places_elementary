8e0a5f5e39562b7b88d92efe6e763be19a0a0a65
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-04-12 23:29:33 +0300

    feat: add PhotoViewScreen

--- lib/features/places/screens/photo_view_screen/photo_view_screen_model.dart
+++ lib/features/places/screens/photo_view_screen/photo_view_screen_model.dart
@@ -1,16 +1,6 @@
 import 'package:elementary/elementary.dart';
-import 'package:places_elementary/features/places/domain/entity/place.dart';
-import 'package:places_elementary/features/places/service/places_service.dart';
 
 /// Default Elementary model for PhotoViewScreen module
 class PhotoViewScreenModel extends ElementaryModel {
-  final PlacesService _placesService;
-
-  PhotoViewScreenModel(
-    ErrorHandler errorHandler,
-    this._placesService,
-  ) : super(errorHandler: errorHandler);
-
-  /// Получить место по id
-  Future<Place> getPlaceDetails(int placeId) => _placesService.getPlaceDetails(placeId);
+  PhotoViewScreenModel(ErrorHandler errorHandler) : super(errorHandler: errorHandler);
 }
--- lib/features/places/screens/photo_view_screen/photo_view_screen_wm.dart
+++ lib/features/places/screens/photo_view_screen/photo_view_screen_wm.dart
@@ -6,13 +6,19 @@ import 'package:places_elementary/features/places/screens/photo_view_screen/phot
 import 'package:provider/provider.dart';
 
 abstract class IPhotoViewScreenWidgetModel extends IWidgetModel {
+  ListenableState<int> get currentImageState;
+
+  PageController get pageController;
+
+  void onPageChanged(int imageIndex);
+
+  void goBack();
 }
 
 PhotoViewScreenWidgetModel defaultPhotoViewScreenWidgetModelFactory(BuildContext context) {
   final appDependencies = context.read<IAppScope>();
   final model = PhotoViewScreenModel(
     appDependencies.errorHandler,
-    appDependencies.placesService,
   );
 
   return PhotoViewScreenWidgetModel(model);
@@ -21,6 +27,40 @@ PhotoViewScreenWidgetModel defaultPhotoViewScreenWidgetModelFactory(BuildContext
 /// Default widget model for PhotoViewScreenWidget
 class PhotoViewScreenWidgetModel extends WidgetModel<PhotoViewScreen, PhotoViewScreenModel>
     implements IPhotoViewScreenWidgetModel {
+  /// Текущая фотография
+  late final StateNotifier<int> _currentImageState;
+  late final PageController _pageController;
+
+  @override
+  ListenableState<int> get currentImageState => _currentImageState;
+
+  @override
+  PageController get pageController => _pageController;
 
   PhotoViewScreenWidgetModel(PhotoViewScreenModel model) : super(model);
+
+  @override
+  void initWidgetModel() {
+    super.initWidgetModel();
+
+    _currentImageState = StateNotifier<int>(initValue: widget.currentIndex);
+    _pageController = PageController(initialPage: widget.currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  void onPageChanged(int imageIndex) {
+    _currentImageState.accept(imageIndex);
+  }
+
+  @override
+  void goBack() {
+    Navigator.of(context).pop();
+  }
 }

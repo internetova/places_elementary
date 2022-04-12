0d3ab79480bd3f72c98d50a036d988d597f660fa
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2022-04-12 23:27:53 +0300

    feat: координата для PhotoViewScreen

--- lib/features/places/coordinates/place_details_coordinate.dart
+++ lib/features/places/coordinates/place_details_coordinate.dart
@@ -1,10 +1,12 @@
 import 'package:places_elementary/features/navigation/domain/entity/coordinate.dart';
 import 'package:places_elementary/features/places/domain/entity/card_type.dart';
 import 'package:places_elementary/features/places/domain/entity/place.dart';
+import 'package:places_elementary/features/places/screens/photo_view_screen/photo_view_screen.dart';
 import 'package:places_elementary/features/places/screens/place_details_screen/place_details_screen.dart';
 
 class PlaceDetailsCoordinate extends Coordinate {
   static const placeDetailsScreen = PlaceDetailsCoordinate._('place_details_screen', false);
+  static const photoViewScreen = PlaceDetailsCoordinate._('photo_view_screen', true);
 
   const PlaceDetailsCoordinate._(
     String value,
@@ -20,4 +22,8 @@ final Map<PlaceDetailsCoordinate, CoordinateBuilder> placeDetailsCoordinates = {
         place: (data as Map)['place'] as Place,
         transitionFrom: data['transitionFrom'] as CardType,
       ),
+  PlaceDetailsCoordinate.photoViewScreen: (_, data) => PhotoViewScreen(
+    images: (data as Map)['images'] as List<String>,
+    currentIndex: data['currentIndex'] as int,
+  ),
 };
